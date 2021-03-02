ae7bd4402ebd4390030a9fe6220c0f43e92535b6
Jack <Decafoats108@gmail.com>
2021-03-02 00:39:03 -0600

    Image, Layout, darkmode and IphoneX safeArea Fixes - Disabled darkmode on both platforms - Widened Green Script area to avoid cutoff - Compatible with IphonX Safearea (hopefully) - Made FireFighter logo background transparent - Disabled Landscape mode

--- Test/Test.Android/MainActivity.cs
+++ Test/Test.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Test.Droid
 {
-    [Activity(Label = "Test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
